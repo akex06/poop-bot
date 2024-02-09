131d917a0cfeeb2413be44603c211c5853c4a7fd
akex06 <nadalessandro06@gmail.com>
2024-02-09 11:40:56 +0100

    fixed the help command shitcode

--- src/help.py
+++ src/help.py
@@ -7,16 +7,21 @@ from discord.ext.commands import Cog, Command
 from src.constants import EMBED_COLOR
 
 
-def get_cog_help_embed(cog: commands.Cog, prefix: str):
+def get_command_description(command: commands.Command, prefix: str, with_ticks: bool = True) -> str:
+    parameters = "".join(
+        f" <{name}>" if parameter.required else f" [{name}]"
+        for name, parameter in command.params.items()
+    )
+
+    tick = "`" if with_ticks else ""
+    print(tick)
+    return f"{tick}{prefix}{command.name}{parameters}{tick} - {command.description}"
+
+
+def get_cog_help_embed(cog: commands.Cog, prefix: str) -> discord.Embed:
     description = list()
     for command in cog.get_commands():
-        parameters = " ".join(
-            f"<{name}>" if parameter.required else f"[{name}]"
-            for name, parameter in command.params.items()
-        )
-        description.append(
-            f"`{prefix}{command.name} {parameters}` - {command.description}"
-        )
+        description.append(get_command_description(command, prefix))
 
     embed = discord.Embed(description="\n".join(description), color=EMBED_COLOR)
     embed.set_author(name=f"{cog.qualified_name} cog")
@@ -24,7 +29,18 @@ def get_cog_help_embed(cog: commands.Cog, prefix: str):
     return embed
 
 
-class HelpSelector(discord.ui.Select):
+def get_command_help_embed(command: commands.Command, prefix: str) -> discord.Embed:
+    description = f"""
+        {prefix}{command.name}
+        ```{get_command_description(command, prefix, with_ticks=False)}```
+    """
+    embed = discord.Embed(description=description, color=EMBED_COLOR)
+    embed.set_author(name=f"{command.name} command")
+
+    return embed
+
+
+class CogHelpSelector(discord.ui.Select):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
 
@@ -56,7 +72,7 @@ class HelpView(discord.ui.View):
         self.ctx = ctx
         self.message: discord.Message | None = None
 
-        self.add_item(HelpSelector(self.ctx.bot))
+        self.add_item(CogHelpSelector(self.ctx.bot))
 
     async def interaction_check(self, interaction: discord.Interaction, /) -> bool:
         if self.ctx.author != interaction.user:
@@ -74,6 +90,8 @@ class HelpView(discord.ui.View):
 
         await self.message.edit(view=self)
 
+    pass
+
 
 class HelpCommand(commands.HelpCommand):
     def __init__(self, **kwargs) -> None:
@@ -86,9 +104,23 @@ class HelpCommand(commands.HelpCommand):
             mapping: Mapping[Optional[Cog], List[Command[Any, ..., Any]]],
             /
     ) -> None:
+        description = """
+            My prefix is `p!`
+            **Help commands**
+            > `p!help` - This help menu
+            > `p!help [cog]` - Cog help
+            > `p!help [command]` - Command help
+        """
+        embed = discord.Embed(description=description, color=EMBED_COLOR)
+        embed.set_author(icon_url=self.context.bot.user.avatar.url, name="PoopBot's help menu")
+
         view = HelpView(self.context)
-        await self.context.send(view=view)
+        view.message = await self.context.send(view=view, embed=embed)
 
     async def send_cog_help(self, cog: Cog, /) -> None:
         embed = get_cog_help_embed(cog, "p!")
+        await self.get_destination().send(embed=embed)
+
+    async def send_command_help(self, command: Command[Any, ..., Any], /) -> None:
+        embed = get_command_help_embed(command, "p!")
         await self.get_destination().send(embed=embed)
